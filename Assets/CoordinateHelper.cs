21ca00e85bc7eecc5d3cef6eb51ab727424cde97
吴志远 <wuzhiyuan@ikingtec.com>
2021-03-16 15:55:59 +0800

    2312312

---
+++ Assets/CoordinateHelper.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ArcGISMapsSDK.Components;
+
+public class CoordinateHelper : MonoBehaviour
+{
+    public static CoordinateHelper _instance;
+    public ArcGISRendererComponent arcGisRender;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+
+
+
+
+    private void Start()
+    {
+
+    }
+
+    public Vector3 EarthCenterPoint;
+    public Vector3 northPoint;
+    public Vector3 northDir;
+    public Vector3 EastDir;
+
+    //设置北极点 center
+    void SetArcGisModelCenter()
+    {
+        double CAH = Camera.main.transform.GetComponent<ArcGISCameraComponent>().Height;
+        EarthCenterPoint = Vector3.down * (6378137f + (float)CAH);
+        northPoint = arcGisRender.ArcGISPointConvertToUnityPoint(0, 90, 0);
+        //Vector3 EastPoint = arcGisRender.ArcGISPointConvertToUnityPoint(0, 0, 0);
+        northDir = (northPoint - EarthCenterPoint).normalized;
+        //EastDir = (EastPoint - EarthCenterPoint).normalized;
+
+        //Debug.Log("地心位置:" + EarthCenterPoint);
+        //Debug.Log("北极点:"+ northPoint);
+        //Debug.Log("北极朝向"+ northDir);
+    }
+
+
+    #region  扩展方法  放到  ArcGISRendererComponent
+    //public Vector3 ArcGISPointConvertToUnityPoint(double lng, double lat, double alt)
+    //{
+    //    var pos = RendererScene.ToGameEngineCoord(localRotation, localOrigin, new ArcGISGlobalCoordinatesPosition(lat, lng, alt));
+    //    return pos.ToVector3();
+    //}
+
+    //public Quaternion ArcGISQuaternionConvertToUnityQuaternion(double lng, double lat, double alt, double heading, double pitch, double roll)
+    //{
+    //    var rotation = RendererScene.ToGameEngineRotation(localOrigin, new ArcGISGlobalCoordinatesPosition(lat, lng, alt),
+    //                                                            new ArcGISRotation(pitch, roll, heading));
+    //    return rotation.ToQuaternion();
+    //}
+    #endregion
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!arcGisRender)
+        {
+            if (GameObject.Find("RenderContainer") != null)
+            {
+                arcGisRender = GameObject.Find("RenderContainer").GetComponent<ArcGISRendererComponent>();
+            }
+        }
+        SetArcGisModelCenter();
+    }
+}
---
+++ Assets/LocationRect.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ArcGISMapsSDK.Components;
+
+public class LocationRect : MonoBehaviour
+{
+    public string A;
+    ArcGISRendererComponent arcGisRender;
+    public GameObject axisObj;//
+    public GameObject axisObj0;//
+    public bool IsActive;
+    public bool IsActive2;
+    public bool IsActive3;
+    public bool IsActive4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!arcGisRender)
+            arcGisRender = GameObject.Find("RenderContainer").GetComponent<ArcGISRendererComponent>();
+        axisObj = this.gameObject;
+
+        if (axisObj0 == null && this.transform.parent.Find("axiGo") == null)
+        {
+            axisObj0 = new GameObject("axiGo");
+            axisObj0.transform.parent = this.transform.parent;
+            axisObj0.transform.localPosition = Vector3.zero;
+            axisObj0.transform.localEulerAngles = this.transform.localEulerAngles;
+        }
+        if (this.transform.parent.Find("axiGo") != null)
+        {
+            axisObj0 = this.transform.parent.Find("axiGo").gameObject;
+        }
+
+        //Rec2();
+    }
+
+
+    public void Rec2()
+    {
+        axisObj0.transform.LookAt(CoordinateHelper._instance.northPoint);
+        axisObj.transform.localEulerAngles = new Vector3(180, -180 + axisObj0.transform.localEulerAngles.y - 0.8f/*- 0.5f*/, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsActive)
+        {
+            float deltaAngle = Vector3.Angle(arcGisRender.transform.right, axisObj.transform.right);
+
+            //北天东坐标系的0，0，0
+            Vector3 angle_nue0 = Vector3.right * -deltaAngle;//heading  pitch
+
+            //Debug.Log(angle_nue0.x);
+            axisObj.transform.localEulerAngles = new Vector3(180, angle_nue0.x, 0);
+        }
+        if (IsActive2)
+        {
+            axisObj0.transform.LookAt(CoordinateHelper._instance.northPoint);
+            //axisObj.transform.localEulerAngles = new Vector3(180, -180 + axisObj0.transform.localEulerAngles.y/* - 0.5f*/, 0);
+            axisObj.transform.localEulerAngles =      new Vector3(axisObj.transform.localEulerAngles.x,  axisObj0.transform.localEulerAngles.y, axisObj.transform.localEulerAngles.z);
+                //axisObj0.transform.localEulerAngles;
+
+
+            //  new Vector3(180, -180 + axisObj0.transform.localEulerAngles.y, -180);
+        }
+        if (IsActive3)
+        {
+            axisObj0.transform.LookAt(CoordinateHelper._instance.northPoint);
+            axisObj.transform.localEulerAngles = new Vector3(180, -180 + axisObj0.transform.localEulerAngles.y, 0);
+        }
+        if (IsActive4)
+        {
+            axisObj0.transform.LookAt(CoordinateHelper._instance.northPoint);
+            axisObj.transform.localEulerAngles = new Vector3(180, -180 + axisObj0.transform.localEulerAngles.y - 0.8f, 0);
+        }
+    }
+
+
+
+
+
+
+
+    private Vector3 GetBetweenPoint(Vector3 start, Vector3 end, float percent = 0.5f)
+    {
+        Vector3 normal = (end - start).normalized;
+        float distance = Vector3.Distance(start, end);
+        return normal * (distance * percent) + start;
+    }
+}
